06f6ebb2c40f76a3c8340a2b16d2eaa5765ea5f5
thiago <thiago.alessandro.farias@gmail.com>
2020-01-20 23:51:50 -0300

    Arquitetura de microserviço

---
+++ src/main/java/br/com/intelector/coremicroservice/CoreMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package br.com.intelector.coremicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoreMicroserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoreMicroserviceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/intelector/coremicroservice/CoreMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.intelector.coremicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoreMicroserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
